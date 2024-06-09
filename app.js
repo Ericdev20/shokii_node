3e2e8e0df6a97ca4b370146ae896494594158318
dev.owomi <erickoalladaye@gmail.com>
2024-06-09 14:57:00 +0100

    MAJ

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
   cors: {
-    origin: ["http://localhost:4200", "https://shokii.com/"],
+    origin: [
+      "http://localhost:4200",
+      "https://shokii.com/",
+      "https://local.shokii.com/",
+    ],
     methods: ["GET", "POST"],
   },
 });
