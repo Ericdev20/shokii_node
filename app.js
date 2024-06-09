d9658cc15a0d9066d8e2fbff7e1a15c9a88b283f
dev.owomi <erickoalladaye@gmail.com>
2024-06-09 14:04:04 +0100

    'MAJ'

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
-
 const io = require("socket.io")(http, {
-  // cors: {
-  //   origin: ["http://localhost:4200", "https://shokii.com/"],
-  //   methods: ["GET", "POST"],
-  // },
-   cors: {
-      origin: '*',
-    }
+  cors: {
+    origin: ["http://localhost:4200", "https://shokii.com/"],
+    methods: ["GET", "POST"],
+  },
+});
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./ServiceAccount.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://shokii-push.firebaseio.com",
 });
 
-const cors = require("cors");
-
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -47,15 +53,51 @@ io.on("connection", (socket) => {
   });
 
   socket.on("disconnected", (userId) => {
-    // const userId = Array.from(onlineUsers).find(
-    //   (id) => id === socket.id
-    // );
     if (userId) {
       console.log(`L'utilisateur avec l'ID ${userId} s'est déconnecté`);
       onlineUsers.delete(userId);
       io.emit("onlineUsers", Array.from(onlineUsers));
     }
   });
+});
+
+const sendNotification = (token, title, body) => {
+  const message = {
+    notification: {
+      title: title,
+      body: body,
+      image: "https://shokii.com/assets/images/logo/shokii.png",
+    },
+    webpush: {
+      fcm_options: {
+        link: "https://shokii.com",
+      },
+    },
+    token: token,
+  };
+
+  admin
+    .messaging()
+    .send(message)
+    .then((response) => {
+      console.log("Successfully sent message:", response);
+    })
+    .catch((error) => {
+      console.error("Error sending message:", error);
+      if (
+        error.errorInfo.code === "messaging/registration-token-not-registered"
+      ) {
+        // Handle the case where the token is not registered
+        console.log("Token is not registered. Handle accordingly.");
+        // Optionally, remove the token from your database
+      }
+    });
+};
+
+app.post("/sendNotification", (req, res) => {
+  const { token, title, body } = req.body;
+  sendNotification(token, title, body);
+  res.status(200).send("Notification envoyée");
 });
 
 const port = 3000;
